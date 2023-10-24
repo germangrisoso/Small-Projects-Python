b3273258dfd191b53a189afbcc265d683296918e
davidgrisoso <germangrisoso@gmail.com>
2023-10-23 23:46:00 -0500

    Blackjack V1 complete

--- blackjack.py
+++ blackjack.py
@@ -66,4 +66,122 @@ def main():
 
         playerValue=getHandValue(playerHand)
         dealerValue=getHandValue(dealerHand)
-             
+
+        if dealerValue>21:
+            print('Dealer busts. You ein ${}'.format(bet))
+            money+=bet
+        elif(playerValue>21) or (playerValue<dealerValue):
+            print('You lost')
+            money-=bet
+        elif playerValue>dealerValue:
+            print('You won ${}!'.format(bet))
+            money+=bet
+        elif playerValue == dealerValue:
+            print("It's a tie,he bet is returned to you")
+        
+        input('Press Enter to continue...')
+        print('\n\n')
+
+def getBet(maxBet):
+
+    while True:
+        print('How much do you bet? (1-{}, or Quit)'.format(maxBet))
+        bet=input('> ').upper().strip()
+        if bet == 'Quit':
+            print('Thanks for playing')
+            sys.exit()
+
+        if not bet.isdecimal():
+            continue
+
+        bet=int(bet)
+
+        if 1<=bet<=maxBet:
+            return bet
+
+def getDeck():
+    deck=[] 
+    for suit in (HEARTS,DIAMONDS,SPADES,CLUBS):
+        for rank in range(2,11) :
+            deck.append((str(rank),suit))
+        for rank in ('J','Q','K','A'):
+            deck.append((rank,suit))
+    random.shuffle(deck)
+    return(deck) 
+
+def displayHands(playerHand,dealerHand,showDealerHand):
+    print()
+    if showDealerHand:
+        print('DEALER:', getHandValue(dealerHand))
+        displayCards(dealerHand)
+    else:
+        print('DEALER:??????')
+        displayCards([BACKSIDE]+dealerHand[1:])
+
+    print('PLAYER:',getHandValue(playerHand))
+    displayCards(playerHand)
+
+
+def getHandValue(cards):
+    value=0
+    numberOfAces=0
+
+    for card in cards:
+        rank=card[0]
+        if rank == 'A':
+            numberOfAces+=1
+        elif rank in ('K','Q','J'):
+            value+=10
+        else:
+            value+=int(rank)
+    
+    value+=numberOfAces
+
+    for i in range(numberOfAces):
+
+        if value+10<=21:
+            value+=10
+    
+    return value
+
+def displayCards(cards):
+
+    rows=['','','','','']
+
+    for i, card in enumerate(cards):
+        rows[0]+=' ___  '
+        if card == BACKSIDE:
+
+            rows[1]+='|## | '
+            rows[2]+='|###| '
+            rows[3]+='|_##| '
+        else:
+            rank,suit=card
+            rows[1]+='|{}  |'.format(rank.ljust(2))
+            rows[2]+='| {} |'.format(suit)
+            rows[3]+='|__{}| '.format(rank.rjust(2,'_'))
+
+    for row in rows:
+        print(row)
+
+
+def getMove(playerHand,money):
+
+    while True:
+        moves=['(H)it','(S)tand']
+
+        if len(playerHand)==2 and money>0:
+            moves.append('(D)ouble down')
+        
+        movePrompt=', '.join(moves) + '> '
+        move=input(movePrompt).upper()
+
+        if move in ('H','S'):
+            return move
+        if move == 'D' and '(D)ouble down' in moves:
+            return move
+        
+if __name__== '__main__':
+    main()
+
+
